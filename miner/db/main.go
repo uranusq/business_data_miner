e48f51ad9261adff09adbc4124378ad2d7df5921
Rust Kam <karust13@gmail.com>
2019-06-11 21:16:26 +0300

    getting URLs from DB

--- miner/db/main.go
+++ miner/db/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -9,13 +11,34 @@ import (
 
 // Database ... Contains methods to work with data
 type Database struct {
-	gorm.DB
+	*gorm.DB
+	busyCrawlIDs  []int
+	busyCollyIDs  []int
+	busyGoogleIDs []int
 }
 
-// PrintInfo ...
+// OpenInitialize ... Open connection to DB and Initializes custom structure. Connection needs to be closed
+func (db *Database) OpenInitialize(path string) {
+	gdb, err := gorm.Open("sqlite3", path)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	//defer gdb.Close()
+
+	gdb.AutoMigrate(&Industries{}, &Companies{})
+	db.DB = gdb
+	db.Exec("PRAGMA foreign_keys = ON;")
+
+	// Exclude 0 indexes, since they always have empty values in SQLite
+	db.busyCollyIDs = []int{0}
+	db.busyCrawlIDs = []int{0}
+	db.busyGoogleIDs = []int{0}
+}
+
+// PrintInfo ... Prints basic info about items in database
 func (db *Database) PrintInfo() {
 	industries := []Industries{}
-	db.Find(&industries)
+	db.DB.Find(&industries)
 	fmt.Println("Industries in DB: ", len(industries))
 
 	companies := []Companies{}
@@ -23,22 +46,78 @@ func (db *Database) PrintInfo() {
 	fmt.Println("Companies in DB: ", len(companies))
 }
 
-func initizalizeDB(path string) *Database {
-	gdb, err := gorm.Open("sqlite3", path)
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer gdb.Close()
+func (db *Database) GetCrawlURL() {
 
-	// Migrate the schema
-	gdb.AutoMigrate(&Industries{}, &Companies{})
-	db := Database{DB: *gdb}
-	fmt.Println(gdb)
-	return &db
+}
+
+func (db *Database) GetCollyURL() (string, error) {
+	company := Companies{}
+	db.Where("id NOT IN (?)", db.busyCollyIDs).Where(&Companies{IsCollyCrawled: false}).Find(&company)
+	if company.ID == 0 {
+		return "", errors.New("[GetCollyURL] no URLs found")
+	}
+	db.busyCollyIDs = append(db.busyCollyIDs, company.ID)
+	fmt.Println("busy colly: ", db.busyCollyIDs)
+	return company.URL, nil
+}
+
+func (db *Database) FinishedCollyURL() {
+
+}
+
+func (db *Database) GetGoogleURL() {
+
+}
+
+func (db *Database) fillToDebug() {
+	testIndustr := []Industries{
+		Industries{Industry: "Internet Services"},
+		Industries{Industry: "Software Developement"},
+		Industries{Industry: "Education"},
+		Industries{Industry: "Retail"},
+	}
+
+	for _, ind := range testIndustr {
+		db.Create(&ind)
+	}
+
+	testCompanies := []Companies{
+		Companies{URL: "https://domru.ru", Industry: "Internet Services"},
+		Companies{URL: "https://innopolis.ru", Industry: "Education"},
+		Companies{URL: "https://tattelecom.ru/", Industry: "Internet Services"},
+		Companies{URL: "https://www.wikipedia.org", Industry: "Education"},
+		Companies{URL: "https://kai.ru", Industry: "Education"},
+		Companies{URL: "https://www.acronis.com", Industry: "Software Developement"},
+		Companies{URL: "https://www.kaspersky.ru", Industry: "Software Developement"},
+		Companies{URL: "http://pivoman-kazan.ru", Industry: "Retail"},
+	}
+	for _, comp := range testCompanies {
+		isPossible := db.NewRecord(&comp)
+		if !isPossible {
+			log.Println("Cannot insert value: ", &comp)
+		}
+		db.Create(&comp)
+	}
 }
 
 func main() {
-	//db := Database{}
-	db := initizalizeDB("test.db")
+	//os.Remove("test.db")
+	db := Database{}
+	db.OpenInitialize("test.db")
+	db.fillToDebug()
+
 	db.PrintInfo()
+	collyURL, err := db.GetCollyURL()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("CollyURL: ", collyURL)
+
+	collyURL, err = db.GetCollyURL()
+	fmt.Println("CollyURL: ", collyURL)
+
+	collyURL, err = db.GetCollyURL()
+	fmt.Println("CollyURL: ", collyURL)
+
+	db.Close()
 }
--- miner/db/table.go
+++ miner/db/table.go
@@ -1,10 +1,9 @@
 package main
 
-import "github.com/jinzhu/gorm"
-
 //Industries ... Industried by `Thomson Reuters Business Classification` and number of files belonging to them
 type Industries struct {
-	gorm.Model
+	//gorm.Model
+	ID       int    `gorm:"primary_key;AUTO_INCREMENT"`
 	Industry string `gorm:"unique;not null"`
 	NumURL   *uint  `gorm:"default:0"`
 	NumHTML  *uint  `gorm:"default:0"`
@@ -13,13 +12,14 @@ type Industries struct {
 
 // Companies ... Companies with URL and other info that belong to some industry
 type Companies struct {
-	gorm.Model
-	URL             *string `gorm:"unique;not null"`
+	//gorm.Model
+	ID              int    `gorm:"primary_key;AUTO_INCREMENT"`
+	URL             string `gorm:"unique;not null"`
 	Name            string
-	IsCommonCrawled *bool       `gorm:"default:0"`
-	IsGoogleFilter  *bool       `gorm:"default:0"`
-	IsCollyCrawled  *bool       `gorm:"default:0"`
-	NumDocs         *uint       `gorm:"default:0"`
-	NumHTML         *uint       `gorm:"default:0"`
-	Industry        *Industries `gorm:"not null"`
+	IsCommonCrawled bool   `gorm:"default:0"`
+	IsGoogleFilter  bool   `gorm:"default:0"`
+	IsCollyCrawled  bool   `gorm:"default:0"`
+	NumDocs         *uint  `gorm:"default:0"`
+	NumHTML         *uint  `gorm:"default:0"`
+	Industry        string `sql:"type:integer REFERENCES Industries(industry)"`
 }
