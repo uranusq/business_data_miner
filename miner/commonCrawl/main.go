750fcbff4612f0411b35cc31c76afcf7a24f00e7
Rustem Kamalov <karust13@gmail.com>
2019-06-14 22:20:06 +0300

    Common crawl finished

--- miner/commonCrawl/main.go
+++ miner/commonCrawl/main.go
@@ -1,88 +1,129 @@
 package main
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
-type IndexAPIRespone struct {
+var indexServer = "http://index.commoncrawl.org/"
+var crawlStorage = "https://commoncrawl.s3.amazonaws.com/"
+
+// IndexAPI ... API Response structure from `index.commoncrawl.org`
+type IndexAPI struct {
+	Urlkey       string `json:"urlkey,omitempty"`
+	Timestamp    string `json:"timestamp,omitempty"`
+	Charset      string `json:"charset,omitempty"`
+	Mime         string `json:"mime,omitempty"`
+	Languages    string `json:"languages,omitempty"`
+	MimeDetected string `json:"mimedetected,omitempty"`
+	Digest       string `json:"digest,omitempty"`
+	Offset       string `json:"offset,omitempty"`
+	URL          string `json:"url,omitempty"`
+	Length       string `json:"length,omitempty"`
+	Status       string `json:"status,omitempty"`
+	Filename     string `json:"filename,omitempty"`
 }
 
-/*
-   Make request to commoncrawl index API to gather all offsets that contain pointed URL
-   Arguments:
-       crawl: Crawl database which should be used, e.g 'CC-MAIN-2019-22';
-       url: URL of site, offsets and other info of which should be returned.
-   Returns a list of JSON objects with information about each file offset and other data.
-*/
-func getIndex(crawl string, url string) {
-	//pages = [json.loads(x) for x in resp.content.strip().split('\n'.encode("utf-8"))]
-	client := http.Client{Timeout: 10 * time.Second}
-	req, err := http.NewRequest("GET", "http://index.commoncrawl.org/"+crawl+"-index", nil)
-	if err != nil {
-		log.Println(err)
-	}
+// GetIndex ... Makes request to commoncrawl index API to gather all offsets that contain pointed URL
+//   crawl: Crawl a database which should be used, e.g 'CC-MAIN-2019-22';
+//   url: URL of a site, offsets and other info of which should be returned.
+// Returns a list of JSON objects with information about each file offset and other data.
+func GetIndex(crawl string, url string) ([]IndexAPI, error) {
+	// Build request
+	client := http.Client{Timeout: 30 * time.Second}
+	req, _ := http.NewRequest("GET", indexServer+crawl+"-index", nil)
 
+	// Add request params and do it
 	q := req.URL.Query()
 	q.Add("url", url)
 	q.Add("output", "json")
 	req.URL.RawQuery = q.Encode()
-
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
+		return nil, fmt.Errorf("getIndex response read error: %v", err)
 	}
-	s := new(IndexAPIRespone)
+
+	// Read response
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err.Error())
+		return nil, fmt.Errorf("getIndex response read error: %v", err)
 	}
-	err = json.Unmarshal(body, &s)
-	if err != nil {
-		fmt.Println("whoops:", err)
+
+	// Parse the response that contains JSON objects seperated with new line
+	rawPages := strings.Split(string(body), "\n")
+	pages := []IndexAPI{}
+	for _, p := range rawPages {
+		val := &IndexAPI{}
+		err := json.NewDecoder(strings.NewReader(p)).Decode(&val)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		pages = append(pages, *val)
 	}
-	//json.NewDecoder(resp.Body).Decode(&result)
-	fmt.Println(s)
-	//return pages
+	return pages, nil
 }
 
-/*
-def saveContent(pages, saveTo):
-    """
-    Saves pages or text that were found in Common Crawl to choosen folder
-        pages: info about found web pages from `getIndex function`
-        saveTo: destination fodler, where save fetched web data
-    """
-    crawlStorage = 'https://commoncrawl.s3.amazonaws.com/'
+// SaveContent ... Saves pages or text that were found in Common Crawl to choosen folder
+//   pages: info about found web pages from `getIndex function`
+//   saveTo: destination fodler, where save fetched web data
+func SaveContent(pages []IndexAPI, saveTo string) error {
+	client := http.Client{Timeout: 30 * time.Second}
 
-    for i, page in enumerate(pages):
-        offset, length = int(page['offset']), int(page['length'])
-        offsetEnd = offset + length - 1
-        resp = requests.get(crawlStorage + page['filename'], headers={'Range': 'bytes={}-{}'.format(offset, offsetEnd)})
+	for i, page := range pages {
+		offset, _ := strconv.Atoi(page.Offset)
+		length, _ := strconv.Atoi(page.Length)
+		offsetEnd := offset + length + 1
 
-        rawData = BytesIO(resp.content)
-        f = gzip.GzipFile(fileobj=rawData)
+		req, _ := http.NewRequest("GET", crawlStorage+page.Filename, nil)
+		req.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", offset, offsetEnd))
 
-        data = f.read()
-        warc, header, response = data.strip().split('\r\n\r\n'.encode("utf-8"), 2)
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("saveContent response read error: %v", err)
+		}
 
-        mime = magic.from_buffer(response, mime=True)
-        ext = mimeExtensions[mime]
-        startURL = warc.find(b'WARC-Target-URI:') + 17
-        endURL = warc.find(b'\r\nWARC-Payload-Digest')
-        url = warc[startURL:endURL].decode("utf-8")
-        urlClear = "".join(["%"+str(ord(c)) if c in ["/", "\\", ":", "?"] else c for c in url])
+		// Read response
+		if err != nil {
+			return fmt.Errorf("saveContent response read error: %v", err)
+		}
+		// Deflate response and split the WARC, HEADER, HTML from it
+		reader, _ := gzip.NewReader(resp.Body)
+		b, err := ioutil.ReadAll(reader)
+		splitted := strings.Split(string(b), "\r\n\r\n")
+		warc := splitted[0]
+		//header := splitted[1]
+		response := splitted[2]
 
-        with open("{0}/{1}{2}".format(saveTo, urlClear, ext), "wb+") as f:
-            f.write(response)
-        print("Processing [{0}]: {1}/{2}".format(url, i+1, len(pages)))
-*/
+		ext := extensionByContent([]byte(response))
+
+		startURL := strings.Index(warc, "WARC-Target-URI:") + 17
+		endURL := strings.Index(warc, "\r\nWARC-Payload-Digest")
+		url := escapeURL(warc[startURL:endURL])
+
+		// Write extracted HTML and show progess
+		ioutil.WriteFile(saveTo+"/"+url+ext, []byte(response), 0644)
+		fmt.Printf("Page %v/%v\n", i+1, len(pages))
+	}
+	return nil
+}
+
+// ChangeIndexServer ... changes default address `http://index.commoncrawl.org/``
+func ChangeIndexServer(server string) {
+	indexServer = server
+}
 
 func main() {
-	getIndex("CC-MAIN-2019-22", "example.com")
-	//saveContent(pages: pages, saveTo: "data")
+	pages, err := GetIndex("CC-MAIN-2019-22", "example.com/")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	SaveContent(pages, "./data")
 }
